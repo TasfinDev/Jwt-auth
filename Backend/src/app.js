344c29ea41c1d0c3eefa021ecabf5f4cd179c68f
Md.Tasfin Islam <md.tasfinislam@MdTasfins-MacBook-Air.local>
2026-07-16 15:34:58 +0600

    refactor: replace hardcoded local api urls

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Allow requests from your frontend dev server
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://jwt-auth-ebon.vercel.app',
   credentials: true,
 }));
 
